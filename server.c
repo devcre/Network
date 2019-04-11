e9412a8975f27c636356183cde86b522e12266c9
devcre <tigertank707@gmail.com>
2019-04-11 22:11:19 +0900

    add server.c

--- server.c
+++ server.c
@@ -15,6 +15,7 @@
 #include <netdb.h>
 
 char *content_type(char *ext);
+char *extract_extension(char *data_for_parse);
 
 void cleanExit(){
     exit(0);
@@ -36,11 +37,15 @@ int main(int argc, char *argv[])
     int portno; // port number
     socklen_t clilen;
 
-    char buffer[256];
-    char resp_buf[1024];
+    char buffer[256]; // data which contain user request
+    char resp_buf[1024]; //data which is going to sent to user
+    char temp_buf[1024]; // if data for transmit is too heavy, send data divided into small size
 
-    // HTTP request data
-    char *pot1, *pot2;
+    // file which wil be opened
+    char *url;
+    char *tem_url;
+
+    // request data type
     char *extension;
 
     // html
@@ -65,10 +70,12 @@ int main(int argc, char *argv[])
     if(sockfd < 0){
         error("ERROR opening socket");
     }
+
     if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]); //atoi converts from String to Integer
     serv_addr.sin_family = AF_INET;
@@ -81,20 +88,20 @@ int main(int argc, char *argv[])
     listen(sockfd,5); // Listen for socket connections. Backlog queue (connections to wait) is 5
 
     clilen = sizeof(cli_addr);
-    /*accept function: 
+    /*accept function:
     1) Block until a new connection is established
     2) the new socket descriptor will be used for subsequent communication with the newly connected client.
     */
 
     while(1){
-        // printf("bzero\n");
-        bzero(buffer,256);
-        bzero(resp_buf,1024);
         //bzero(extension,256);
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (newsockfd < 0){
             error("ERROR on accept");
         }
+        bzero(buffer,256);
+        bzero(temp_buf,1024);
+        bzero(resp_buf,1024);
 
         n = read(newsockfd,buffer,255); //Read is a block function. It will read at most 255 bytes
         if (n < 0) error("ERROR reading from socket");
@@ -107,47 +114,61 @@ int main(int argc, char *argv[])
             cleanExit();
         }
 
-        // parse page data and get URL
-        pot1 = strtok(buffer," ");
-        pot1 = strtok(NULL, " ");
-        printf("%s\n", pot1);
-        if(strcmp(pot1,"/")==0){
+        // extract file extension
+        url = extract_extension(buffer); // url = "~~~.~~"
+        printf("strcmp(url, imissyou.gif): %d\n", strcmp(url, "imissyou.gif"));
+
+        if((url == NULL) /*|| (strcmp(url, "page.html") != 0) || (strcmp(url, "imissyou.gif") != 0) */){
             sprintf(resp_buf, "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\nContent-Type: text/plain\r\n\r\n");
             n = write(newsockfd,resp_buf,strlen(resp_buf));
             printf("%s\n", resp_buf);
             if (n < 0) error("ERROR writing to socket");
         }
         else{
-            pot2 = strtok(pot1, ".");
-            pot2 = strtok(NULL,"");
-            printf("%s\n", pot2);
-            // pot = strtok(NULL, " \r\n");
-            extension = content_type(pot2);
-
-            // size of html file
-            fp = fopen("page.html", "r");
-            fpp = fopen("page.html", "r");
+            // size of file
+            fp = fopen(url, "r");
             fseek(fp, 0, SEEK_END);
             file_size = ftell(fp);
+            fclose(fp);
+
+            // file pointer for write
+            fpp = fopen(url, "r");
 
             char resp_data[file_size];
             bzero(resp_data, file_size);
             data_size = fread(resp_data, 1, file_size, fpp);
 
+            // determine Content-Type
+            tem_url = strtok(url, "."); // tem_url = ""~~~.""
+            printf("tem_url: %s\n", tem_url);
+            tem_url = strtok(NULL, "");
+            printf("tem_url: %s\n", tem_url);
+            extension = content_type(tem_url);
+
             // response message
             sprintf(resp_buf, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: %s\r\n\r\n", file_size, extension);
             printf("%s\n",resp_buf);
+
             // send response message
+            n = write(newsockfd, resp_buf, strlen(resp_buf));
+            if(n < 0) error("ERROR writing to socket\n");
+            // n = write(newsockfd, resp_buf, strlen(resp_buf)); //NOTE: write function returns the number of bytes actually sent out Ñ> this might be less than the number you told it to send
+            // if (n < 0) error("ERROR writing to socket");
+            // printf("%d",n);
 
-            n = write(newsockfd, resp_buf, strlen(resp_buf)); //NOTE: write function returns the number of bytes actually sent out Ñ> this might be less than the number you told it to send
-            if (n < 0) error("ERROR writing to socket");
-            printf("%d",n);
+            // send data
+            while(feof(fpp) == 0){ // if fpp doesn't point end of file, feof function returns 0
+                printf("data\n");
+                fread(resp_data, 1, 512, fpp);
+                printf("data2\n");
+                n = write(newsockfd, resp_data, strlen(resp_data)); 
+                printf("data3\n");
+                if (n < 0) error("ERROR writing to socket");
+                printf("data4\n");
+                //bzero(resp_data,512);
+                printf("data5\n");
+            }
 
-            // transfer html file data
-            n = write(newsockfd, resp_data, data_size); 
-            if (n < 0) error("ERROR writing to socket");
-
-            fclose(fp);
             fclose(fpp);
         }
     }
@@ -181,3 +202,22 @@ char *content_type(char *ext){
     }
 }
 
+char *extract_extension(char *data_for_parse){
+    char *pot1;
+    char *empty;
+
+    printf("data_for_parse: %s\n", data_for_parse);
+    pot1 = strtok(data_for_parse, " /"); // pot1 = "GET"
+    printf("pot1_1: %s\n", pot1);
+    pot1 = strtok(NULL, " /"); // pot1 = "/~~~.~~"
+    printf("pot1_2: %s\n", pot1);
+    // no request file
+    if(strcmp(pot1, "HTTP") == 0){
+        return NULL;
+    }
+    printf("url: %s\n", pot1);
+
+    return pot1;
+
+}
+// why no header file???
